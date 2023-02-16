68faa88e39db1dea152eadaf67823bdfb2f47f50
Tokaallah <tokallah.92@gmail.com>
2023-02-16 03:43:53 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - Entry point
  *
- * Return: always 1 
+ * Return: always 1 (success)
  */
 int main(void)
 {
-	write(2, 
+	write(2,
 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
