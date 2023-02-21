9021ec5f70bca89a916ee975bda9e3372f8a9017
Tokaallah <tokallah.92@gmail.com>
2023-02-21 07:30:10 -0800

    13 natural made the numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0;
+ */
+int main(void)
+{
+	int sum3 = 0, sum5 = 0;
+	int i;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 )
+		{
+			sum3 += i;
+		}
+		if (i % 5 == 0 )
+		{
+			sum5 += i;
+		}
+	}
+	printf("%d\n", sum3 + sum5);
+	return (0);
+}
