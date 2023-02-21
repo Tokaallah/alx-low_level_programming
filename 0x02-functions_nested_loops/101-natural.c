46415d3753c1a73cd30bca26de8ebf621366af8b
Tokaallah <tokallah.92@gmail.com>
2023-02-21 07:33:51 -0800

    Q13 edit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,17 +5,21 @@
  *
  * Return: always 0;
  */
+
 int main(void)
 {
 	int sum3 = 0, sum5 = 0;
 	int i;
+
 	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0 )
+
+		if (i % 3 == 0)
 		{
 			sum3 += i;
 		}
-		if (i % 5 == 0 )
+
+		if (i % 5 == 0)
 		{
 			sum5 += i;
 		}
