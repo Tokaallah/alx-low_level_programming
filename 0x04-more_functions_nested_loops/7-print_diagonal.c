0bb1a80ea8c85a546e6e7a30b7aaa66ab8d970fe
Tokaallah <tokallah.92@gmail.com>
2023-02-23 02:51:09 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: times diagonal line is printed
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			putchar(' ');
+		}
+		putchar('\\');
+	}
+	putchar('\n');
+}
