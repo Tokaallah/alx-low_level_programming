9435ac959e3387e590b4809682131b3d4494d00b
Tokaallah <tokallah.92@gmail.com>
2023-02-23 01:36:56 -0800

    1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
  * _isdigit - checks for a digit
  *
  * @c: if c is a digit
- * 
- * Returns 1 or 0
+ *
+ * Return: 1 or 0
  */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return(1);
+		return (1);
 	return (0);
 }
