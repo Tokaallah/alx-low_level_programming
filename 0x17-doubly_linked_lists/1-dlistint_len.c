990d9ad237d4b0118b43a81a3132406857bd92e3
Tokaallah <tokallah.92@gmail.com>
2023-05-18 09:39:10 +0000

    print number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlist_len - print number elements of a dlistint.
+ * @h: type dlistint point of prev and next node
+ * Return: count.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    const dlistint_t *current = h;
+    size_t count = 0;
+
+    while (current != NULL)
+    {
+        count++;
+        current = current->next;
+    }
+
+    return count;
+}
